fc6ac02d861e184262fd103f6075a0f76c910892
Leandro <leandrodsg@gmail.com>
2026-01-15 00:21:10 +0100

    feat: initialize Next.js project with TypeScript and Tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,29 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-plugin-prettier';
+import security from 'eslint-plugin-security';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      prettier: prettier,
+      security: security,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      ...security.configs.recommended.rules,
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
