68cfa7dd724548d6cd427e284b0d7d8a310e7d8a
Leandro <leandrodsg@gmail.com>
2026-01-15 12:15:43 +0100

    Integrate AboutTherapist component into main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from '@/components/sections/Hero';
 import ProblemStatement from '@/components/sections/ProblemStatement';
 import SoundFamiliar from '@/components/sections/SoundFamiliar';
+import { AboutTherapist } from '@/components/sections/AboutTherapist';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <ProblemStatement />
       <SoundFamiliar />
+      <AboutTherapist />
     </main>
   );
 }
